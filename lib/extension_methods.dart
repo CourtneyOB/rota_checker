b7c1a4d8a365a30aadfc58a26d822162df579fb9
Courtney <courtneyobeirne@gmail.com>
2022-01-26 16:19:48 +0000

    addition of extensions for parsing to string

--- lib/extension_methods.dart
+++ lib/extension_methods.dart
@@ -2,6 +2,29 @@ extension toString on DateTime {
   String dateFormatToString() {
     return '${this.hour < 10 ? '0${this.hour}' : this.hour}:${this.minute < 10 ? '0${this.minute}' : this.minute}';
   }
+
+  String dayOfWeekToString() {
+    List<String> days = ['Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat', 'Sun'];
+    return days[this.weekday - 1];
+  }
+
+  String monthToString() {
+    List<String> months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    return months[this.month - 1];
+  }
 }
 
 extension dateOnlyCompare on DateTime {
