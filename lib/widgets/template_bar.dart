7df31d8a6cf753467253c90b8c74f632ee8a70d8
Courtney <courtneyobeirne@gmail.com>
2022-01-30 17:43:17 +0000

    highlight text when button inactive

--- lib/widgets/template_bar.dart
+++ lib/widgets/template_bar.dart
@@ -1,43 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:rota_checker/constants.dart';
-import 'package:rota_checker/custom_scroll_behaviour.dart';
 import 'package:rota_checker/main.dart';
-import 'package:rota_checker/model/on_call_template.dart';
-import 'package:rota_checker/model/shift.dart';
-import 'package:rota_checker/model/shift_template.dart';
-import 'package:rota_checker/widgets/template_card.dart';
-import 'package:rota_checker/model/template.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rota_checker/widgets/text_icon_button.dart';
+import 'package:rota_checker/widgets/template_list.dart';
 
-class TemplateBar extends ConsumerWidget {
-  final ScrollController controller = ScrollController();
+class TemplateBar extends ConsumerStatefulWidget {
+  @override
+  TemplateBarState createState() => TemplateBarState();
+}
+
+class TemplateBarState extends ConsumerState<TemplateBar> {
+  double fontSize = 14.0;
+  Color fontColour = kText;
+  FontWeight fontWeight = FontWeight.normal;
+  Duration animationDuration = Duration(milliseconds: 600);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    Template? selectedTemplate = ref.watch(dataProvider).selectedTemplate;
-    List<Template> templateLibrary = ref.watch(dataProvider).templateLibrary;
-
-    List<TemplateCard> templates = templateLibrary
-        .map((item) => TemplateCard(
-              name: item.name,
-              colour: item.colour,
-              startTime: item.startTime,
-              endTime: item.endTime,
-              isOnCall: item is OnCallTemplate ? true : false,
-              expectedHours: item is OnCallTemplate ? item.expectedHours : null,
-              isNight: item is ShiftTemplate ? item.isNight : false,
-              isLong: item is ShiftTemplate ? item.isLong : false,
-              isEveningFinish:
-                  item is ShiftTemplate ? item.isEveningFinish : false,
-              isSelected: item == selectedTemplate ? true : false,
-              onPress: () {
-                ref
-                    .read(dataProvider.notifier)
-                    .selectTemplate(templateLibrary.indexOf(item));
-              },
-            ))
-        .toList();
-
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         border: Border(
@@ -52,51 +32,60 @@ class TemplateBar extends ConsumerWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  ref.watch(dataProvider).selectedTemplate == null
-                      ? 'Choose a Template'
-                      : 'Select dates',
-                  style: TextStyle(color: kText),
-                ),
-                if (ref.watch(dataProvider).selectedTemplate != null &&
-                    ref.watch(dataProvider).selectedDates.isNotEmpty)
-                  TextButton(
-                    style: TextButton.styleFrom(
-                        padding: EdgeInsets.zero,
-                        minimumSize: Size.zero,
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap),
-                    child: Text('Apply'),
-                    onPressed: () {},
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: AnimatedDefaultTextStyle(
+                    duration: animationDuration,
+                    curve: Curves.ease,
+                    style: TextStyle(
+                        color: fontColour,
+                        fontSize: fontSize,
+                        fontWeight: fontWeight),
+                    child: Text(
+                      ref.watch(dataProvider).selectedTemplate == null
+                          ? 'Choose a Template'
+                          : 'Select dates',
+                    ),
                   ),
+                ),
+                TextIconButton(
+                  text: 'Apply',
+                  colour: kDarkPrimary,
+                  icon: Icons.check,
+                  onPress: ref.watch(dataProvider).selectedTemplate != null &&
+                          ref.watch(dataProvider).selectedDates.isNotEmpty
+                      ? () {
+                          print('tap!');
+                        }
+                      : () {
+                          //Enlarge and decrease the size of the text informing the user what to do
+                          setState(() {
+                            fontWeight = FontWeight.bold;
+                            fontColour = Colors.red;
+                            fontSize = 14.5;
+                          });
+                          //Return text style to normal following completion of the animation
+                          Future.delayed(animationDuration, () {
+                            setState(() {
+                              fontSize = 14.0;
+                              fontColour = kText;
+                              fontWeight = FontWeight.normal;
+                            });
+                          });
+                        },
+                  isActive: ref.watch(dataProvider).selectedTemplate != null &&
+                          ref.watch(dataProvider).selectedDates.isNotEmpty
+                      ? true
+                      : false,
+                ),
               ],
             ),
           ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border(top: BorderSide(color: kLightGrey)),
-                color: kBackground,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ScrollConfiguration(
-                  behavior: CustomScrollBehavior(),
-                  child: ListView.builder(
-                      controller: controller,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: templates.length,
-                      itemBuilder: (BuildContext context, index) {
-                        return templates[index];
-                      }),
-                ),
-              ),
-            ),
-          ),
+          TemplateList(),
         ],
       ),
     );
